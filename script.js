aec77839504bdfe4273fcbc7905fa9beb4b31612
Gunukula Rakesh <gunukularakesh7@gmail.com>
2026-02-23 12:48:58 +0530

    commit changes

--- script.js
+++ script.js
@@ -420,8 +420,8 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const recognition = new SpeechRecognition();
 
 recognition.lang = 'en-US';
-recognition.continuous = true;
-recognition.interimResults = false;
+recognition.continuous = false;
+recognition.interimResults = true;
 
 recognition.onresult = (event) => {
   let command = event.results[event.results.length - 1][0].transcript.toLowerCase();
